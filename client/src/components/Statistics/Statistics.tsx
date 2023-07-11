272a0aacb0267ef0b3b1bd7cc988e6a308192c98
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-07-11 17:26:54 -0300

    staistics

--- client/src/components/Statistics/Statistics.tsx
+++ client/src/components/Statistics/Statistics.tsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { use } from "react";
 import style from "./style.module.css";
+import useStadistics from "@/hooks/useStadistics";
 
-function Statistics(stadistics: any) {
+function Statistics() {
+	const {
+		productsSoldInMonth,
+		gramsSoldInMonth,
+		productSoldMostInMonth,
+		productSoldMostInYear,
+		top10ProductsByYear,
+		top10ProductsByMonth,
+	} = useStadistics();
+
 	return (
 		<div className={style.container}>
 			<div className={style.card}>
 				<h3 className={style.h3}>Productos</h3>
 				<h4 className={style.h4}>vendidos en el mes</h4>
-				<p>124</p>
+				<p>{productsSoldInMonth}</p>
 			</div>
 			<div className={style.card}>
 				<h3 className={style.h3}>Gramos</h3>
 				<h4 className={style.h4}>vendidos en el mes</h4>
-				<p>1300</p>
+				<p>{gramsSoldInMonth}</p>
 			</div>
 			<div className={style.card}>
-				<h3 className={style.h3}>Presupuestos</h3>
-				<h4 className={style.h4}>calculados en el mes</h4>
-				<p>5</p>
+				<h3 className={style.h3}>Producto</h3>
+				<h4 className={style.h4}>mas vendido en el mes</h4>
+				<p>{productSoldMostInMonth?.code || ""}</p>
+			</div>
+			<div className={style.card}>
+				<h3 className={style.h3}>Producto</h3>
+				<h4 className={style.h4}>mas vendido en el año</h4>
+				<p>{productSoldMostInYear?.code || ""}</p>
 			</div>
 		</div>
 	);
---
+++ client/src/hooks/useStadistics.ts
@@ -0,0 +1,96 @@
+import { BudgetProduct } from "@/types/types";
+import useBudget from "./useBudget";
+
+function useStadistics() {
+	const { budgetList: budgets } = useBudget();
+
+	// Cantidad de productos vendidos en el mes
+	const productsSoldInMonth = budgets.reduce((count, budget) => {
+		return (
+			count +
+			budget.products.reduce((productCount, product) => {
+				return productCount + product.quantity;
+			}, 0)
+		);
+	}, 0);
+
+	// Cantidad de gramos vendidos en el mes
+	const gramsSoldInMonth = budgets.reduce((count, budget) => {
+		return (
+			count +
+			budget.products.reduce((gramsCount, product) => {
+				return gramsCount + product.quantity * product.weight;
+			}, 0)
+		);
+	}, 0);
+
+	// Producto más vendido del mes
+	const soldProductsInMonth = budgets.flatMap((budget) => budget.products);
+
+	const productSoldMostInMonth = soldProductsInMonth.reduce(
+		(mostSoldProduct: BudgetProduct | null, product) => {
+			if (!mostSoldProduct || product.quantity > mostSoldProduct.quantity) {
+				return product;
+			}
+			return mostSoldProduct;
+		},
+		null
+	);
+
+	// Producto más vendido del año
+	const productSoldMostInYear = soldProductsInMonth.reduce(
+		(mostSoldProduct: BudgetProduct | null, product) => {
+			if (!mostSoldProduct || product.quantity > mostSoldProduct.quantity) {
+				return product;
+			}
+			return mostSoldProduct;
+		},
+		null
+	);
+
+	// Top 10 de productos más vendidos en el año
+	const productSalesByYear: Record<string, number> = soldProductsInMonth.reduce(
+		(salesByProduct: Record<string, number>, product) => {
+			if (!salesByProduct[product.code]) {
+				salesByProduct[product.code] = 0;
+			}
+			salesByProduct[product.code] += product.quantity;
+			return salesByProduct;
+		},
+		{}
+	);
+
+	const top10ProductsByYear = Object.entries(productSalesByYear)
+		.sort((a, b) => b[1] - a[1])
+		.slice(0, 10)
+		.map(([code, sales]) => ({ code, sales }));
+
+	// Top 10 de productos más vendidos en el mes
+	const productSalesByMonth: Record<string, number> = soldProductsInMonth.reduce(
+		(salesByProduct: Record<string, number>, product) => {
+			if (!salesByProduct[product.code]) {
+				salesByProduct[product.code] = 0;
+			}
+			salesByProduct[product.code] += product.quantity;
+			return salesByProduct;
+		},
+		{}
+	);
+
+	const top10ProductsByMonth = Object.entries(productSalesByMonth)
+		.sort((a, b) => b[1] - a[1])
+		.slice(0, 10)
+		.map(([code, sales]) => ({ code, sales }));
+
+	// Devuelve las estadísticas o cualquier otro valor que desees exponer como resultado del hook
+	return {
+		productsSoldInMonth,
+		gramsSoldInMonth,
+		productSoldMostInMonth,
+		productSoldMostInYear,
+		top10ProductsByYear,
+		top10ProductsByMonth,
+	};
+}
+
+export default useStadistics;
