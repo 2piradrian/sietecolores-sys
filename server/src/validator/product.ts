af51b339c79ad308201deeed02dfd1b130394e62
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-08-15 15:59:20 -0300

    product validations

--- server/src/validator/product.ts
+++ server/src/validator/product.ts
@@ -9,6 +9,10 @@ export const ProductValidator = {
 		next();
 	},
 	update(req: Request, res: Response, next: NextFunction) {
+		const { id } = req.params;
+		if (!id) {
+			return res.status(400).json({ error: "Missing params" });
+		}
 		const { name, weight, size, code, type } = req.body;
 		if (!name || !weight || !size || !code || !type) {
 			return res.status(400).json({ error: "Missing params" });
@@ -16,9 +20,17 @@ export const ProductValidator = {
 		next();
 	},
 	delete(req: Request, res: Response, next: NextFunction) {
+		const { id } = req.params;
+		if (!id) {
+			return res.status(400).json({ error: "Missing params" });
+		}
 		next();
 	},
 	getById(req: Request, res: Response, next: NextFunction) {
+		const { id } = req.params;
+		if (!id) {
+			return res.status(400).json({ error: "Missing params" });
+		}
 		next();
 	},
 	getAll(req: Request, res: Response, next: NextFunction) {
